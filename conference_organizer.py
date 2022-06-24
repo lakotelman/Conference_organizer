93b239ab3b04e4f86da4c840e4e80fe38a9c336f
lakotelman <la.n.kot@pm.me>
2022-06-24 12:35:55 -0800

    More notes

--- conference_organizer.py
+++ conference_organizer.py
@@ -29,6 +29,9 @@ class Conference_Org():
             "United Kingdom": [], 
             "France": []
         }
+#Date math is majority of work. Once dates are extracted, store those as another attriubute for the consecutive availability dates. 
+#Example Person_A and Person_B are both available on June1-June2 those attributes would be comparable 
+#Using counter you could determine how many people have the attribute for each date-pair. 
 
         api_link = "https://ct-mock-tech-assessment.herokuapp.com/"
         alldata = requests.get(api_link).json()["partners"]
@@ -50,7 +53,7 @@ class Conference_Org():
 
 #*** Getting to the Availability Lists  
 # Loop through the attributes of each person in each country to find the lists of available dates 
-    # address_book --> country --> person object --> availability list
+    # address_book --> country --> person objects in the list --> availability list
 #TODO Code to redesign for this task
 # delta = datetime.timedelta(days=1)
 
@@ -68,6 +71,7 @@ class Conference_Org():
 #           "United Kingdom": {(date-pair): [person objects who have this availability]
 #                              (date-pair): [person objects who have this availability]}
 #                           }
+#Alternatively we could set the consecutive dates as another attribute within the Person Class-Object
 # From these lists we construct a dictionary of date-pairs in the lists (organized by country)
     # UK --> {opt1: [], opt: []} (We would need to make sure these tuples for the date-pairs are not added multiple times)
 # I wonder if there is a way to add the person to the list at the same time as constructing the key and value? 
