cb14110683778a859c06123c0a99a7fb3d66ce99
MeilaniNishimura <meilaninishimura@gmail.com>
2022-06-24 19:20:26 -0500

    added start_days attribute to person

--- conference_organizer.py
+++ conference_organizer.py
@@ -6,12 +6,13 @@ import datetime
 
 
 class Person: 
-    def __init__(self, firstname, lastname, email, country, availability): 
+    def __init__(self, firstname, lastname, email, country, availability, start_days): 
         self.firstname = firstname
         self.lastname =lastname
         self.email = email
         self.country = country
         self.availability = availability
+        self.start_days = start_days
 
     def __repr__(self): 
         return f"{self.firstname} {self.lastname}"
@@ -43,26 +44,29 @@ class Conference_Org():
                 email = partner["email"],
                 country = partner["country"],
                 # This is a list of datetime objects and calls function which shows its date. Datetime sucks.
-                availability = [datetime.datetime.strptime(date, '%Y-%m-%d').date() for date in partner["availableDates"]]
+                availability = [datetime.datetime.strptime(date, '%Y-%m-%d').date() for date in partner["availableDates"]],
+                start_days = []
             )
+            
+            delta = datetime.timedelta(days=1)
+            for index, days in enumerate(p.availability[:-1]):
+                if p.availability[index + 1] - p.availability[index] == delta:
+                    p.start_days.append(p.availability[index])
+            else: 
+                pass
+           
             for k in self.address_book.keys(): 
                 if k == p.country:
                     self.address_book[k].append(p)
 
-
+    def compare_dates():
 
 #*** Getting to the Availability Lists  
 # Loop through the attributes of each person in each country to find the lists of available dates 
-    # address_book --> country --> person objects in the list --> availability list
+    # address_book --> country --> person objects in the list --> start_days
 #TODO Code to redesign for this task
-# delta = datetime.timedelta(days=1)
 
-# for index, days in enumerate(person2[:-1]):
-#     if person2[index + 1] - person2[index] == delta:
-#         tups[person1[index], person1[index+1]] = []
-#         tups[person1[index], person1[index+1].append(person1)
-#     else: 
-#         pass
+
 
 #*** Finding and storing the Date-Pairs (maybe populating attendees as well?) 
 #  Date-Pairs-By-Country: { 
