1e4345c2287ae0e687b26ba5ed5be01a6fa89633
lakotelman <la.n.kot@pm.me>
2022-06-24 16:25:08 -0800

    Added Method to get common dates

--- conference_organizer.py
+++ conference_organizer.py
@@ -3,6 +3,7 @@
 import requests
 from pprint import pprint
 import datetime 
+from collections import Counter
 
 
 class Person: 
@@ -30,9 +31,6 @@ class Conference_Org():
             "United Kingdom": [], 
             "France": []
         }
-#Date math is majority of work. Once dates are extracted, store those as another attriubute for the consecutive availability dates. 
-#Example Person_A and Person_B are both available on June1-June2 those attributes would be comparable 
-#Using counter you could determine how many people have the attribute for each date-pair. 
 
         api_link = "https://ct-mock-tech-assessment.herokuapp.com/"
         alldata = requests.get(api_link).json()["partners"]
@@ -59,28 +57,43 @@ class Conference_Org():
                 if k == p.country:
                     self.address_book[k].append(p)
 
-    def compare_dates():
+    def find_common_start_dates(self):
+        self.start_days_by_country = { 
+            "United States":[],
+            "Ireland": [],
+            "Spain": [], 
+            "Mexico":[], 
+            "Canada": [],
+            "Singapore": [], 
+            "Japan": [], 
+            "United Kingdom": [], 
+            "France": []
+        }
+        
+        for country, person in self.address_book.items():
+            for person in self.address_book[country]:
+                self.start_days_by_country[country].extend(person.start_days)
+        
+        for country, person in self.start_days_by_country.items(): 
+            print(country, max(Counter(self.start_days_by_country[country]), key=Counter(self.start_days_by_country[country]).get))
 
-#*** Getting to the Availability Lists  
-# Loop through the attributes of each person in each country to find the lists of available dates 
-    # address_book --> country --> person objects in the list --> start_days
-#TODO Code to redesign for this task
 
 
 
-#*** Finding and storing the Date-Pairs (maybe populating attendees as well?) 
-#  Date-Pairs-By-Country: { 
-#            "United States": {(date-pair): [person objects who have this availability]
-#                              (date-pair): [person objects who have this availability]}
-#           "United Kingdom": {(date-pair): [person objects who have this availability]
-#                              (date-pair): [person objects who have this availability]}
-#                           }
-#Alternatively we could set the consecutive dates as another attribute within the Person Class-Object
-# From these lists we construct a dictionary of date-pairs in the lists (organized by country)
-    # UK --> {opt1: [], opt: []} (We would need to make sure these tuples for the date-pairs are not added multiple times)
-# I wonder if there is a way to add the person to the list at the same time as constructing the key and value? 
 
-#*** Picking the right date-pair for each country and constructing the Post Request
-# Compare the lengths of the lists to determine which date pair contains the most partners in each country
-# Construct a dictionary from this information with start date, country, and attendees emails
-# Do something to check it with the API?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- scratch.py
+++ scratch.py
@@ -1,3 +1,23 @@
 
 
-from datetime import datetime
+#*** Getting to the Availability Lists  
+# Loop through the attributes of each person in each country to find the lists of available dates 
+    # address_book --> country --> person objects in the list --> start_days
+#TODO Code to redesign for this task
+
+#*** Finding and storing the Date-Pairs (maybe populating attendees as well?) 
+#  Date-Pairs-By-Country: { 
+#            "United States": {(date-pair): [person objects who have this availability]
+#                              (date-pair): [person objects who have this availability]}
+#           "United Kingdom": {(date-pair): [person objects who have this availability]
+#                              (date-pair): [person objects who have this availability]}
+#                           }
+#Alternatively we could set the consecutive dates as another attribute within the Person Class-Object
+# From these lists we construct a dictionary of date-pairs in the lists (organized by country)
+    # UK --> {opt1: [], opt: []} (We would need to make sure these tuples for the date-pairs are not added multiple times)
+# I wonder if there is a way to add the person to the list at the same time as constructing the key and value? 
+
+#*** Picking the right date-pair for each country and constructing the Post Request
+# Compare the lengths of the lists to determine which date pair contains the most partners in each country
+# Construct a dictionary from this information with start date, country, and attendees emails
+# Do something to check it with the API?
